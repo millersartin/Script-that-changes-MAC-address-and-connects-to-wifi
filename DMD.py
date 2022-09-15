de7abcf51626a901903e4863c28d27db47fcc457
wuzaaa <65517098+wuzaaa@users.noreply.github.com>
2022-09-15 18:21:08 +0000

    Update DMD.py

--- DMD.py
+++ DMD.py
@@ -2,8 +2,8 @@ import os
 
 os.system('ifconfig wlp3s0 down')
 print('\nwlp3s0 is down\n')
-os.system('macchanger -m 3C:58:C2:E5:41:F1 wlp3s0')
+os.system('macchanger -m xx:xx:xx:xx:xx:xx wlp3s0')
 print('\n')
 os.system('warp-cli connect')
 print('\n')
-os.system('nmcli d wifi connect WCS-DMD')
+os.system('nmcli d wifi connect wifi_SSID_here')
